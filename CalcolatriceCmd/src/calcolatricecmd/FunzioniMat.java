d2b184509a4b778755fe622b9906b93b74d24b25
assun <assun@LAPTOP-26936G13>
2022-10-20 17:37:39 +0200

    Creata la classe Funzioni Mat (funzioni  matematiche) con all'interno il metodo che applica il teorema di pitagora

---
+++ CalcolatriceCmd/src/calcolatricecmd/FunzioniMat.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package calcolatricecmd;
+        
+import static java.lang.Math.sqrt;
+
+
+/**
+ *
+ * @author assun
+ */
+public class FunzioniMat {
+
+    public FunzioniMat() {
+    }
+    
+    //metodo che, date le lunghezze dei due cateti di un 
+    //triangolo rettangolo, restituisce la lunghezza dell'ipotenusa
+    public double pitagora(int a, int b) {
+        return Math.sqrt((a*a) + (b*b));
+    }
+    
+}
