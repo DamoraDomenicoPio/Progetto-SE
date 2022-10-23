8e2b61b3f69bc8594428fa1bd3c79bc97420fe44
assun <assun@LAPTOP-26936G13>
2022-10-23 19:27:12 +0200

    metodi statici, in particolare la classe teorema

--- CalcolatriceCmd/src/calcolatricecmd/teorema.java
+++ CalcolatriceCmd/src/calcolatricecmd/teorema.java
@@ -11,23 +11,14 @@ import static java.lang.Math.sqrt;
  *
  * @author assun
  */
-public class FunzioniMat {
+public class teorema {
 
-    public FunzioniMat() {
-    }
     
     //metodo che, date le lunghezze dei due cateti di un 
     //triangolo rettangolo, restituisce la lunghezza dell'ipotenusa
-    public double pitagora(double a, double b) {
+    public static double pitagora(double a, double b) {
         return Math.sqrt((a*a) + (b*b));
     }
     
-    public double seno(double angolo) {
-        return Math.sin(angolo);
-    }
-    
-    public double coseno(double angolo) {
-        return Math.cos(angolo); 
-    }
     
 }
