e01a59639afc931eb44e4bfc787705b1f763f7b2
assun <assun@LAPTOP-26936G13>
2022-10-23 19:19:51 +0200

    Aggiunta la classe funzioneMatematica che contiene i metodi radiceQuadraca e logaritmo10

---
+++ CalcolatriceCmd/src/calcolatricecmd/funzioneMatematica.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package calcolatricecmd;
+import java.lang.Math.*;
+
+/**
+ *
+ * @author assun
+ */
+public class funzioneMatematica {
+    
+    
+    public static double radiceQuadrata(double x) {
+        return Math.sqrt(x);
+    }
+    
+    //log_10(a) = log_e(a)/log_e(10)
+    public static double logaritmo10(double x) {
+        return Math.log(x)/Math.log(10);
+    }
+}
