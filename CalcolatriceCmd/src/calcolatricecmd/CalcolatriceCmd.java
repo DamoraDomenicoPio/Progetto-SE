36739a3407a791b47ac2ff4be61906e3ff952e68
DOMENICO PIO D'AMORA <domypio001@gmail.com>
2022-10-23 20:00:22 +0200

    Classe main

--- CalcolatriceCmd/src/calcolatricecmd/CalcolatriceCmd.java
+++ CalcolatriceCmd/src/calcolatricecmd/CalcolatriceCmd.java
@@ -21,44 +21,63 @@ public class CalcolatriceCmd {
         double a=0.0, b=0.0;
         Scanner scan = new Scanner(System.in);
         System.out.println("*** CALCOLATRICE ***\n");
-        System.out.println("Quale operazione intendi fare?");
-        System.out.print("1. Pitagora * Inserire 2 cateti per conoscere l'ipotemusa *\n"
-                + "2. Seno * Inserisci un angolo in radianti per conoscere il seno *\n"
-                + "3. Coseno * Inserisci un angolo in radianti per conoscere il coseno *\n"
-                + "4. Radice quadrata * Inserisci un numero per conoscere la sua radice quadrata *\n"
-                + "5. Logaritmo * Inserisci un numero per conoscere il suo logaritmo in base 10\n");
-        i=scan.nextInt();
-        switch(i) {
-            case 1:
-                System.out.println("Inserisci due numeri");
-                a=scan.nextDouble();
-                b=scan.nextDouble();
-                //Teorema.pitagora(a, b);
-                System.out.println("Risultato: ");
-                break;
-            case 2:
-                System.out.println("Inserisci un numero");
-                a=scan.nextDouble();
-                System.out.println("Risultato: ");
-                break;
-            case 3:
-                System.out.println("Inserisci un numero");
-                a=scan.nextDouble();
-                System.out.println("Risultato: ");
-                break;
-            case 4:
-                System.out.println("Inserisci un numero");
-                a=scan.nextDouble();
-                System.out.println("Risultato: ");
-                break;
-            case 5:
-                System.out.println("Inserisci un numero");
-                a=scan.nextDouble();
-                System.out.println("Risultato: ");
-                break;
-            default:
-                System.out.println("odice non valido");
-      }
+        while (i!=-1){
+            System.out.println("Quale operazione intendi fare?");
+            System.out.print("1. Pitagora * Inserire 2 cateti per conoscere l'ipotemusa *\n"
+                    + "2. Seno * Inserisci un angolo in radianti per conoscere il seno *\n"
+                    + "3. Coseno * Inserisci un angolo in radianti per conoscere il coseno *\n"
+                    + "4. Radice quadrata * Inserisci un numero per conoscere la sua radice quadrata *\n"
+                    + "5. Logaritmo * Inserisci un numero per conoscere il suo logaritmo in base 10\n"
+                    + "6. Tangente * Inserisci un angolo in radianti per conoscere la tangente *\n"
+                    + "7. Cotangente * Inserisci un angolo in radianti per conoscere la cotangente *\n");
+            i=scan.nextInt();
+            switch(i) {
+                case 1:
+                    System.out.println("Inserisci due numeri");
+                    a=scan.nextDouble();
+                    b=scan.nextDouble();
+                    //Teorema.pitagora(a, b);
+                    System.out.println("Risultato: " + Funzioni.pitagora(a, b));
+                    break;
+                case 2:
+                    System.out.println("Inserisci un numero");
+                    a=scan.nextDouble();
+                    System.out.println("Risultato: " + Funzioni.seno(a));
+                    break;
+                case 3:
+                    System.out.println("Inserisci un numero" + Funzioni.coseno(a));
+                    a=scan.nextDouble();
+                    System.out.println("Risultato: ");
+                    break;
+                case 4:
+                    System.out.println("Inserisci un numero");
+                    a=scan.nextDouble();
+                    System.out.println("Risultato: " + Funzioni.radicaQuadrata(a));
+                    break;
+                case 5:
+                    System.out.println("Inserisci un numero");
+                    a=scan.nextDouble();
+                    System.out.println("Risultato: " + Funzioni.logaritmo10(a));
+                    break;
+                case 6:
+                    System.out.println("Inserisci un numero");
+                    a=scan.nextDouble();
+                    System.out.println("Risultato: " + Funzioni.tangente(a));
+                    break;
+                case 7:
+                    System.out.println("Inserisci un numero");
+                    a=scan.nextDouble();
+                    System.out.println("Risultato: " + Funzioni.cotangente(a));
+                    break;
+                default:
+                    if(i!=-1){
+                        System.out.println("Codice non valido");
+                    }
+                    System.out.println();
+                    
+            }
+        }
+        
                 
     }
     
