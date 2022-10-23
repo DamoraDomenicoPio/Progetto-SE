a4db5cbdf56fc3394dc66ff50d548ee048a2810b
DOMENICO PIO D'AMORA <domypio001@gmail.com>
2022-10-23 19:33:16 +0200

    Push test

--- CalcolatriceCmd/src/calcolatricecmd/CalcolatriceCmd.java
+++ CalcolatriceCmd/src/calcolatricecmd/CalcolatriceCmd.java
@@ -3,6 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package calcolatricecmd;
+import calcolatricecmd.*;
+import java.util.Scanner;
 
 /**
  *
@@ -15,7 +17,42 @@ public class CalcolatriceCmd {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("hxjwd");
+        int i=0;
+        double a=0.0, b=0.0;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("*** CALCOLATRICE ***\n");
+        System.out.println("Quale operazione intendi fare?");
+        System.out.print("1. Pitagora * Inserire 2 cateti per conoscere l'ipotemusa *\n"
+                + "2. Seno * Inserisci un angolo in radianti per conoscere il seno *\n"
+                + "3. Coseno * Inserisci un angolo in radianti per conoscere il coseno *\n"
+                + "4. Radice quadrata * Inserisci un numero per conoscere la sua radice quadrata *\n"
+                + "5. Logaritmo * Inserisci un numero per conoscere il suo logaritmo in base 10\n");
+        i=scan.nextInt();
+        switch(i) {
+            case 1:
+                System.out.println("Inserisci due numeri");
+                Teorema.pitagora(a, b)
+                break;
+            case 2:
+                System.out.println("Inserisci un numero");
+                
+                break;
+            case 3:
+                System.out.println("Inserisci un numero");
+                
+                break;
+            case 4:
+                System.out.println("Inserisci un numero");
+                
+                break;
+            case 5:
+                System.out.println("Inserisci un numero");
+                
+                break;
+            default:
+              // code block
+      }
+                
     }
     
 }
