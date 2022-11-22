8be8a4d8e09551f8b6f06271c93216ea5771ec71
xAspox <a.dellaporta28@studenti.unisa.it>
2022-11-22 09:56:01 +0100

    Aggiunte le classi Shape e Rectangle

---
+++ SE-Project/src/se/project/figures/Rectangle.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package se.project.figures;
+
+/**
+ *
+ * @author assun
+ */
+public class Rectangle extends Shape {
+    int height; 
+    int width; 
+    
+    
+}
