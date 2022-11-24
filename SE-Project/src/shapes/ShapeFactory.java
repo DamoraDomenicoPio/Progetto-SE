5d81f52145b82a4665a2e4a9990f50367e637184
DOMENICO PIO D'AMORA <d.damora3@studenti.unisa.it>
2022-11-24 13:02:00 +0100

    Add EllipseAdapterTest, LineAdapterTest, RectangleAdapterTest

--- SE-Project/src/shapes/ShapeFactory.java
+++ SE-Project/src/shapes/ShapeFactory.java
@@ -44,4 +44,5 @@ public class ShapeFactory {
         
         return null;
     }
+
 }
---
+++ SE-Project/test/RectangleAdapterTest.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.*;
+import newShapes.RectangleAdapter;
+import org.junit.*;
+import static org.junit.Assert.*;
+import shapes.*;
+import shapes.ShapeFactory;
+
+/**
+ *
+ * @author Domenico
+ */
+public class RectangleAdapterTest {
+    
+    public RectangleAdapterTest() {
+    }
+    
+
+    
+    @Test
+    public void testToString(){
+        RectangleAdapter r =new RectangleAdapter(145.0,171.0,155.0,127.0,Paint.valueOf("0xffffffff"),Paint.valueOf("0x000000ff"),1.0);
+        assertEquals(r.toString(),"Rectangle;145.0;171.0;155.0;127.0;0xffffffff;0x000000ff;1.0");
+    }
+    
+    @Test
+    public void testStringToRectangle(){
+        Rectangle r=RectangleAdapter.stringToRectangle("145.0;171.0;155.0;127.0;0xffffffff;0x000000ff;1.0");
+        
+        assertTrue(145.0==r.getX());
+        assertTrue(171.0==r.getY());
+        assertTrue(155.0==r.getWidth());
+        assertTrue(127.0==r.getHeight());
+        assertTrue("0xffffffff".equals(r.getFill().toString()));
+        assertTrue("0x000000ff".equals(r.getStroke().toString()));
+        assertTrue(1.0==r.getStrokeWidth());
+        
+    }
+}
