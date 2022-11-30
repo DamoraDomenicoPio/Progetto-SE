eac8a6473198dddc70b5170a7cce05954d10d298
xAspox <a.dellaporta28@studenti.unisa.it>
2022-11-30 16:18:44 +0100

    Delete-Command implemented

--- SE-Project/src/command/DeleteCommand.java
+++ SE-Project/src/command/DeleteCommand.java
@@ -4,28 +4,34 @@
  */
 package command;
 
+import javafx.scene.Group;
 import javafx.scene.shape.Shape;
 
 /**
  *
- * @author gianl
+ * @author assun
  */
 public class DeleteCommand implements Command{
     
     private Shape shape;
+    private Group group; 
 
-    public DeleteCommand(Shape shape) {
+    public DeleteCommand(Group group, Shape shape) {
+        this.group = group; 
         this.shape = shape;
     }
     
     @Override
     public void execute() {
-       
+       if(shape !=null){
+            group.getChildren().remove(shape);
+        }
     }
 
     @Override
     public void undo() {
-        
+        System.out.println("undo delete command");
+        this.group.getChildren().add(shape);
     }
     
 }
